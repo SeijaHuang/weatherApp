99310eef311d928ccd187d2cec45d8c298e7fbfd
Shijia Huang <hsj12544975@gmail.com>
2024-06-05 15:50:08 +1000

    docs: put background img of CurrentCity into the assets directory

--- src/components/WeatherCard/components/CurrentCity/CurrentCity.js
+++ src/components/WeatherCard/components/CurrentCity/CurrentCity.js
@@ -5,7 +5,7 @@ import Temperature from "./component/Temperature";
 import TemperatureRange from "./component/TemperatureRange";
 import WeatherIcon from "./component/WeatherIcon";
 import Meta from "./component/Meta";
-import CloudImage from "./background/Cloudy_day_background.png";
+import CloudImage from "./assets/background/Cloudy_day_background.png";
 const CurrentCity = () => {
   return (
     <div className=" container flex h-[100%] w-[100%] flex-col items-center justify-between rounded-[2rem] bg-gradient-to-b from-[#81abfc] to-[#3d7ff9]  p-[1.5rem]">
@@ -15,7 +15,7 @@ const CurrentCity = () => {
         alt=""
       />
       <Date />
-      <CityName cityName="Shanghai" className="text-[2rem] text-white"/>
+      <CityName cityName="Shanghai" className="text-[2rem] text-white" />
       <div className="text-center">
         <Temperature />
         <TemperatureRange />
